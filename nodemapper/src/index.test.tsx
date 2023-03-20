053b800b83b3915b10f569b49f2e00a8185eb10f
John Brittain <jsb1982@gmail.com>
2023-03-20 14:32:31 +0000

    Exclude test files from tsconfig.json

--- nodemapper/src/index.test.tsx
+++ nodemapper/src/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import App from './gui/App'
+import App from '../gui/App'
 
 // using jest for testing
 test('renders learn react link', () => {
