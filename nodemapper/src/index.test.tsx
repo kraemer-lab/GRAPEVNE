624a3975ccfa25aa97cc66a519d931b95d96886b
John Brittain <jsb1982@gmail.com>
2023-03-20 21:02:30 +0000

    Jest fix #1

--- nodemapper/src/index.test.tsx
+++ nodemapper/src/index.test.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
-import App from '../gui/App'
+import { render, screen } from "@testing-library/react";
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
-// using jest for testing
-test('renders learn react link', () => {
-  render(<App />);
+import App from "./gui/App"
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(1 + 2).toBe(3);
+});
+
+test('code quality', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 });
