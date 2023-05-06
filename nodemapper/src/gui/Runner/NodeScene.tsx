4fabee6fdc6d0bd0a3dfeef020fe164e93d2d2a5
John Brittain <john.brittain@dtc.ox.ac.uk>
2023-05-06 14:25:43 +0100

    (wip) Add Builder to nodemapper

--- nodemapper/src/gui/Runner/NodeScene.tsx
+++ nodemapper/src/gui/Runner/NodeScene.tsx
@@ -1,114 +1,7 @@
-import { Component, StrictMode, useRef, useState } from 'react'
-import { createRoot } from 'react-dom/client'
-
-import createEngine from '@projectstorm/react-diagrams'
 import { DiagramModel } from '@projectstorm/react-diagrams'
-import { DiagramEngine } from '@projectstorm/react-diagrams'
-import { DagreEngine } from '@projectstorm/react-diagrams'
+import NodeSceneBase from './NodeSceneBase'
 
-import { DefaultNodeModel } from  'NodeMapComponents'
-import { DefaultLinkModel } from  'NodeMapComponents'
-
-import { BodyWidget } from './BodyWidget'
-
-class NodeScene {
-  engine: DiagramEngine;
-  nodelist = {}
-
-  constructor() {
-    this.InitializeScene();
-  }
-
-  addNode(name: string, color: string, pos: Array<number>, config: JSON = {} as JSON) {
-    const config_str = JSON.stringify(config)
-    const node = new DefaultNodeModel(name, color, config_str);
-    node.setPosition(pos[0], pos[1]);
-    this.engine.getModel().addNode(node);
-    return node;
-  }
-
-  addNodeWithCode(name: string, color: string, pos: Array<number>, code: string) {
-    const config = JSON.parse(JSON.stringify({ 'code': code }));
-    return this.addNode(name, color, pos, config);
-  }
-
-  addLink(port_from: any, port_to: any) {  // eslint-disable-line @typescript-eslint/no-explicit-any
-    const link = new DefaultLinkModel();
-    link.setSourcePort(port_from);
-    link.setTargetPort(port_to);
-    this.engine.getModel().addLink(link);
-  }
-
-  getNodeUserConfig(node) {
-    return JSON.parse(node.options.extras)
-  }
-
-  isNodeTypeRule(node) {
-    return this.getNodeUserConfig(node).type == 'rule'
-  }
-
-  getNodeName(node) {
-    return this.getNodeUserConfig(node).name
-  }
-
-  InitializeScene() {
-    // Initialise Node drawing engine and specify starting layout
-    this.engine = createEngine();
-    const model = new DiagramModel();
-    this.engine.setModel(model);
-
-    /*if ( false ) {
-      const node1 = this.addNode('Input', 'rgb(192,255,0)', [200, 118]);
-      node1.addOutPort('out-1');
-      node1.addOutPort('out-2');
-      node1.addOutPort('out-3');
-      node1.addOutPort('out-4');
-
-      const node2 = this.addNode('Process 1', 'rgb(0,192,255)', [325, 100]);
-      node2.addInPort('in-1');
-      node2.addInPort('in-2');
-      node2.addOutPort('out-1');
-      node2.addOutPort('out-2');
-
-      const node3 = this.addNode('Process 2', 'rgb(0,192,255)', [500, 80]);
-      node3.addInPort('in-1');
-      node3.addInPort('in-2');
-
-      const node4 = this.addNode('Logging', 'rgb(192,0,255)', [500, 150]);
-      node4.addInPort('in-1');
-      node4.addInPort('in-2');
-      node4.addInPort('in-3');
-      node4.addInPort('in-4');
-
-      this.addLink(node1.getPort('out-1'), node2.getPort('in-1'));
-      this.addLink(node2.getPort('out-1'), node3.getPort('in-1'));
-      this.addLink(node2.getPort('out-2'), node4.getPort('in-1'));
-      this.addLink(node1.getPort('out-4'), node4.getPort('in-2'));
-    }*/
-  }
-
-  distributeModel(model) {
-    const dagre_engine = new DagreEngine({
-			graph: {
-				rankdir: 'LR',
-				rankSep: 200,
-        marginx: 100,
-        marginy: 100,
-			},
-		});
-    dagre_engine.redistribute(model)
-    this.engine.repaintCanvas();
-  }
-
-  loadModel(str) {
-    const model = new DiagramModel();
-    model.deserializeModel(JSON.parse(str), this.engine);
-    this.engine.setModel(model);
-  }
-
-  serializeModel() {
-    return JSON.stringify(this.engine.getModel().serialize());
-  }
+class NodeScene extends NodeSceneBase {
 
   buildMapWithSnippets(data: JSON) {
     const model = new DiagramModel();
@@ -179,4 +72,4 @@ class NodeScene {
   }
 }
 
-export default NodeScene;
+export default NodeScene
---
+++ nodemapper/src/gui/Runner/NodeSceneBase.tsx
@@ -0,0 +1,89 @@
+import { Component } from 'react'
+import { StrictMode } from 'react'
+import { useRef } from 'react'
+import { useState } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import createEngine from '@projectstorm/react-diagrams'
+import { DiagramModel } from '@projectstorm/react-diagrams'
+import { DiagramEngine } from '@projectstorm/react-diagrams'
+import { DagreEngine } from '@projectstorm/react-diagrams'
+
+import { DefaultNodeModel } from  'NodeMapComponents'
+import { DefaultLinkModel } from  'NodeMapComponents'
+
+import { BodyWidget } from './BodyWidget'
+
+class NodeSceneBase {
+  engine: DiagramEngine;
+  nodelist = {}
+
+  constructor() {
+    this.InitializeScene();
+  }
+
+  addNode(name: string, color: string, pos: Array<number>, config: JSON = {} as JSON) {
+    const config_str = JSON.stringify(config)
+    const node = new DefaultNodeModel(name, color, config_str);
+    node.setPosition(pos[0], pos[1]);
+    this.engine.getModel().addNode(node);
+    return node;
+  }
+
+  addNodeWithCode(name: string, color: string, pos: Array<number>, code: string) {
+    const config = JSON.parse(JSON.stringify({ 'code': code }));
+    return this.addNode(name, color, pos, config);
+  }
+
+  addLink(port_from: any, port_to: any) {  // eslint-disable-line @typescript-eslint/no-explicit-any
+    const link = new DefaultLinkModel();
+    link.setSourcePort(port_from);
+    link.setTargetPort(port_to);
+    this.engine.getModel().addLink(link);
+  }
+
+  getNodeUserConfig(node) {
+    return JSON.parse(node.options.extras)
+  }
+
+  isNodeTypeRule(node) {
+    return this.getNodeUserConfig(node).type == 'rule'
+  }
+
+  getNodeName(node) {
+    return this.getNodeUserConfig(node).name
+  }
+
+  InitializeScene() {
+    // Initialise Node drawing engine and specify starting layout
+    this.engine = createEngine();
+    const model = new DiagramModel();
+    this.engine.setModel(model);
+  }
+
+  distributeModel(model) {
+    const dagre_engine = new DagreEngine({
+			graph: {
+				rankdir: 'LR',
+				rankSep: 200,
+        marginx: 100,
+        marginy: 100,
+			},
+		});
+    dagre_engine.redistribute(model)
+    this.engine.repaintCanvas();
+  }
+
+  loadModel(str) {
+    const model = new DiagramModel();
+    model.deserializeModel(JSON.parse(str), this.engine);
+    this.engine.setModel(model);
+  }
+
+  serializeModel() {
+    return JSON.stringify(this.engine.getModel().serialize());
+  }
+
+}
+
+export default NodeSceneBase;
