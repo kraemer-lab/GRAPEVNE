73e7415b24bc456c6c5c3ed02bd33a2308115504
John Brittain <jsb1982@gmail.com>
2023-05-15 14:11:06 +0100

    Update jest config

--- nodemapper/run.sh
+++ nodemapper/run.sh
@@ -5,11 +5,5 @@ pushd $SCRIPT_DIR
 
 yarn
 
-# Check dockerfile for explanation of this bit
-if [ ! -d "react-diagrams" ]; then
-	git clone https://github.com/projectstorm/react-diagrams.git
-fi
-cp -r react-diagrams/packages/* node_modules/@projectstorm/
-
 yarn start
 popd
