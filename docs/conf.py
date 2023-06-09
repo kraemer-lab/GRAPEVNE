88f9fd8642f2adca7a75c66a9cd4812bd34caca9
John Brittain <jsb1982@gmail.com>
2023-06-09 15:32:17 +0100

    Add ReadTheDocs theme to sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,9 @@ author = "John Brittain"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx_rtd_theme",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -21,5 +23,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
