a3e3ec24a35edd8266bec7d6b4c578cb4b5ebf0c
John Brittain <jsb1982@gmail.com>
2023-11-08 17:35:52 +0000

    Hash key for entries in module list

--- nodemapper/src/gui/Builder/components/RepoBrowser.tsx
+++ nodemapper/src/gui/Builder/components/RepoBrowser.tsx
@@ -43,6 +43,17 @@ const SelectStyled = styled.select({
   boxSizing: "border-box",
 });
 
+const hash = (s: string) => {
+  let hash = 0, i, chr;
+  if (s.length === 0) return hash;
+  for (i = 0; i < s.length; i++) {
+    chr = s.charCodeAt(i);
+    hash = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
+
 const RepoBrowser = () => {
   const dispatch = useAppDispatch();
   const modules = useAppSelector((state) => state.builder.modules_list);
@@ -74,7 +85,7 @@ const RepoBrowser = () => {
       )
       .map((m) => (
         <TrayItemWidget
-          key={m["name"]}
+          key={hash(JSON.stringify(m))}
           model={m}
           name={m["name"]}
           color={BuilderEngine.GetModuleTypeColor(m["type"])}
