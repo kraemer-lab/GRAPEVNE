37355ff1f137c0de7c72470ff980e2d9e1db6088
John Brittain <jsb1982@gmail.com>
2024-06-25 15:48:50 +0100

    Replace \n with space in collect_stdlibs output

--- electron-app/collect_stdlibs.py
+++ electron-app/collect_stdlibs.py
@@ -4,4 +4,4 @@ reject_list = ["antigravity"]
 
 modules = set(sys.stdlib_module_names) - set(reject_list)
 
-print("\n".join([f"   --collect-all {m}" for m in modules]))
+print(' '.join([f"--collect-all {m}" for m in modules]))
