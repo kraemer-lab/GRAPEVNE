613a0a1b802dc2d4a480948467907a325a498498
John Brittain <jsb1982@gmail.com>
2024-07-01 23:56:19 +0100

    (nodemapper) Remove REST support

--- nodemapper/src/redux/actions/display.ts
+++ nodemapper/src/redux/actions/display.ts
@@ -1,6 +1,5 @@
 import { createAction } from '@reduxjs/toolkit';
 
-export const displayDeleteResults = createAction('display/delete-results');
 export const displayGetFolderInfo = createAction('display/get-folder-info');
 export const displayUpdateNodeInfo = createAction<string>('display/update-node-info');
 export const displaySaveNodeparams = createAction<string>('display/save-nodeparams');
