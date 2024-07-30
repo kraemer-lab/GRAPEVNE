f57416f5671c62dc4772c88e4cc2412b89fb3853
John Brittain <jsb1982@gmail.com>
2024-07-30 14:39:07 +0100

    Update installation paths

--- electron-app/install_miniforge.sh
+++ electron-app/install_miniforge.sh
@@ -8,14 +8,14 @@ if [[ "$RUNNER_OS" == "Windows" ]]; then
     # curl does not work on git-bash, so use the activated python environment
     source "$(poetry env info --path)\\Scripts\\activate"
     python -c "import requests; open('Miniforge3-Windows-x86_64.exe', 'wb').write(requests.get('https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Windows-x86_64.exe', allow_redirects=True).content)"
-    powershell.exe -Command 'Start-Process .\Miniforge3-Windows-x86_64.exe -ArgumentList "/S /NoRegistry=1 /D=$env:UserProfile\miniforge" -Wait'
-    export CONDA_PATH="${USERPROFILE}"/miniforge/condabin
+    powershell.exe -Command 'Start-Process .\Miniforge3-Windows-x86_64.exe -ArgumentList "/S /NoRegistry=1 /D=$env:UserProfile\miniforge3" -Wait'
+    export CONDA_PATH="${USERPROFILE}"/miniforge3/condabin
     echo "done."
 elif [[ "$RUNNER_OS" == "Linux" ]]; then
     echo "Downloading Miniforge for Linux..."
     wget -O Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
     bash Miniforge3.sh -b
-    export CONDA_PATH="${HOME}"/miniforge/bin
+    export CONDA_PATH="${HOME}"/miniforge3/bin
     echo "done."
 elif [[ "$RUNNER_OS" == "macOS" || "$RUNNER_OS" == "Darwin" ]]; then
     # Check for existing installation (will only be present on developer machines)
@@ -26,7 +26,7 @@ elif [[ "$RUNNER_OS" == "macOS" || "$RUNNER_OS" == "Darwin" ]]; then
         curl -fsSLo Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-$(uname -m).sh"
     fi
     bash Miniforge3.sh -b
-    export CONDA_PATH="${HOME}"/miniforge/bin
+    export CONDA_PATH="${HOME}"/miniforge3/bin
     echo "done."
 else
     echo "Unknown OS: $RUNNER_OS"
