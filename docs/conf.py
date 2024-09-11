b6216720e25a59d13d42459d65c3c4f9e41ba2a8
John Brittain <jsb1982@gmail.com>
2024-09-11 15:34:47 +0100

    Adopt sphinx 'book' theme

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,8 @@ exclude_patterns = [
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
+html_logo = "images/logo-wide.png"
 html_static_path = [
     "_static",
 ]
