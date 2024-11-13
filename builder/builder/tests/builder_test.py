5218c4ef621132884e669926013c3f0fbe9081af
John Brittain <jsb1982@gmail.com>
2024-11-13 14:36:02 +0000

    Support binary files in mock

--- builder/builder/tests/builder_test.py
+++ builder/builder/tests/builder_test.py
@@ -19,7 +19,8 @@ class RequestMock:
 
     def __init__(self, status_code, text):
         self.status_code = status_code
-        self.text = text
+        self.text = text  # for requests.get().text
+        self.content = text  # for requests.get().content
 
 
 def test_BuildSnakefile():
