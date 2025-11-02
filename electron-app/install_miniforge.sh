764b07ab98542c857caaa843bb0b0695db764f4a
John Brittain <john.brittain@dtc.ox.ac.uk>
2025-11-02 00:00:03 +0000

    Re-enable mamba install

--- electron-app/install_miniforge.sh
+++ electron-app/install_miniforge.sh
@@ -3,7 +3,7 @@
 set -eoux pipefail
 
 # Bundle miniforge
-# if [[ "$RUNNER_OS" == "Windows" ]]; then
+if [[ "$RUNNER_OS" == "Windows" ]]; then
     echo "Downloading Miniforge for Windows..."
     # curl does not work on git-bash, so use the activated python environment
     source ".venv\\Scripts\\activate"
@@ -14,24 +14,24 @@ set -eoux pipefail
     powershell.exe -Command 'Start-Process .\Miniforge3-Windows-x86_64.exe -ArgumentList "/S /NoRegistry=1 /D=$env:UserProfile\miniforge3" -Wait'
     export CONDA_PATH="${USERPROFILE}"/miniforge3/condabin
     echo "done."
-# elif [[ "$RUNNER_OS" == "Linux" ]]; then
-#     echo "Downloading Miniforge for Linux..."
-#     wget -O Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
-#     bash Miniforge3.sh -b
-#     export CONDA_PATH="${HOME}"/miniforge3/bin
-#     echo "done."
-# elif [[ "$RUNNER_OS" == "macOS" || "$RUNNER_OS" == "Darwin" ]]; then
-#     # Check for existing installation (will only be present on developer machines)
-#     echo "Downloading Miniforge for macOS..."
-#     if [ -f "Miniforge3.sh" ]; then
-#         echo "Miniforge3.sh already exists, skipping download."
-#     else
-#         curl -fsSLo Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-$(uname -m).sh"
-#     fi
-#     bash Miniforge3.sh -b
-#     export CONDA_PATH="${HOME}"/miniforge3/bin
-#     echo "done."
-# else
-#     echo "Unknown OS: $RUNNER_OS"
-#     exit 1
-# fi
+elif [[ "$RUNNER_OS" == "Linux" ]]; then
+    echo "Downloading Miniforge for Linux..."
+    wget -O Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+    bash Miniforge3.sh -b
+    export CONDA_PATH="${HOME}"/miniforge3/bin
+    echo "done."
+elif [[ "$RUNNER_OS" == "macOS" || "$RUNNER_OS" == "Darwin" ]]; then
+    # Check for existing installation (will only be present on developer machines)
+    echo "Downloading Miniforge for macOS..."
+    if [ -f "Miniforge3.sh" ]; then
+        echo "Miniforge3.sh already exists, skipping download."
+    else
+        curl -fsSLo Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-$(uname -m).sh"
+    fi
+    bash Miniforge3.sh -b
+    export CONDA_PATH="${HOME}"/miniforge3/bin
+    echo "done."
+else
+    echo "Unknown OS: $RUNNER_OS"
+    exit 1
+fi
